2326172529e69dcbc4e0a612fa6e1e4b254476de
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-04 17:03:49 -0500

    prod server

---
+++ server.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import express from "express"
+import { handler } from "./dist/server/entry.mjs"
+
+const app = express()
+app.use(express.static("dist/client/"))
+app.use(handler)
+
+const port = process.env.PORT || 42069
+app.listen(port, () => {
+  console.info(`listening on http://localhost:${port}`)
+})
