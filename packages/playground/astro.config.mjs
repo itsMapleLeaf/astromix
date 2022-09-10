fd056cd4bafa96b7693d56f7aabf28c41f1b1cb8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-10 09:29:18 -0500

    upgrades

--- packages/playground/astro.config.mjs
+++ packages/playground/astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-
-import preact from "@astrojs/preact";
+import node from "@astrojs/node"
+import preact from "@astrojs/preact"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node(),
-  integrations: [preact()]
-});
+  integrations: [preact()],
+})
